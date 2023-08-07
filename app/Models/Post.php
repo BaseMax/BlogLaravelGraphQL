3ea15acb4ab2af67f6f3b5ca40c97c6a2f4dadb8
Ali Ahmadi <aliahmadi.github@gmail.com>
2023-08-07 20:07:59 +0330

    start working on graphql part

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "author");
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, "category");
+    }
 }
