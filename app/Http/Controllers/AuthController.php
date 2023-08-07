df88d8d13bfad3caa468a91a06640014ddc31eac
Ali Ahmadi <aliahmadi.github@gmail.com>
2023-08-07 06:52:43 +0330

    login and register in rest api format

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,23 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return "login is here";
+        $request->validated($request->all());
+        if (!Auth::attempt($request->only("email", "password"))) {
+            return $this->error("", "Credentials do not match", 401);
+        }
+
+        $user = User::where("email", $request->email)->first();
+        return $this->success([
+            "user" => $user,
+            "token" => $user->createToken("API token for " . $user->name)->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request)
     {
         $request->validated($request->all());
-        
+
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+            "username" => $request->username
+        ]);
+
+        return $this->success([
+            "user" => $user,
+            "token" => $user->createToken("API token for " . $user->name)->plainTextToken
+        ]);
     }
 
     public function logout()
--- app/Http/Requests/LoginUserRequest.php
+++ app/Http/Requests/LoginUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUserRequest extends FormRequest
+class LoginUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,10 +22,8 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "name"     => ["required", "string", "max:255"],
-            "email"    => ["required", "string", "max:255", "unique:users"],
-            "username" => ["required", "string", "max:255", "unique:users"],
-            "password" => ["required", "confirmed", "min:8"],
+            "email" => ["required", "string", "email"],
+            "password" => ["required", "min:8", "string"]
         ];
     }
 }
